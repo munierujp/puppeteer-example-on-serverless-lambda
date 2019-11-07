0600a8cc96530948834eb3bb78b1b397dedb14b2
Munieru <20086673+munierujp@users.noreply.github.com>
2019-11-07 12:14:50 +0900

    Improve error response

--- index.js
+++ index.js
@@ -10,12 +10,14 @@ exports.handler = async (event, context, callback) => {
   } = event.queryStringParameters || {}
 
   if (!url.match(process.env.ALLOW_URL_PATTERN)) {
-    const message = `${url} does not match to ${process.env.ALLOW_URL_PATTERN}`
-    console.error(message)
-    return callback(null, {
+    console.error(`${url} does not match to ${process.env.ALLOW_URL_PATTERN}`)
+    const resp = createErrorResponse({
       statusCode: 400,
-      body: message
+      type: 'https://github.com/munierujp/ogp-api/blob/master/index.js',
+      title: 'Invalid URL',
+      detail: `${url} is not allowed URL.`
     })
+    return callback(null, resp)
   }
 
   let chrome
@@ -54,20 +56,23 @@ exports.handler = async (event, context, callback) => {
 
     const screenshotBuffer = await page.screenshot()
 
-    return callback(null, {
+    const resp = {
       statusCode: 200,
       isBase64Encoded: true,
       headers: {
         'Content-Type': 'image/png'
       },
       body: screenshotBuffer.toString('base64')
-    })
+    }
+    return callback(null, resp)
   } catch (err) {
     console.error(err)
-    return callback(null, {
+    const resp = createErrorResponse({
       statusCode: 500,
-      body: err
+      type: 'https://github.com/munierujp/ogp-api/blob/master/index.js',
+      title: 'Failed to capture screenshot.'
     })
+    return callback(null, resp)
   } finally {
     if (page) {
       await page.close()
@@ -80,5 +85,24 @@ exports.handler = async (event, context, callback) => {
     if (chrome) {
       await chrome.kill()
     }
+  }
+}
+
+/**
+* Create error response conforming to RFC 7807
+* see https://tools.ietf.org/html/rfc7807
+*/
+function createErrorResponse ({ statusCode, type, title, detail }) {
+  const body = JSON.stringify({
+    type,
+    title,
+    detail
+  })
+  return {
+    statusCode,
+    headers: {
+      'Content-Type': 'application/problem+json'
+    },
+    body
   }
 }
